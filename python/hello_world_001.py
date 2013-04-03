f3e14c3f48baf0589c1bc86e514af75cba1b7836
tonnytg <tonnytg@gmail.com>
2013-04-03 23:20:39 +0000

    file was deleted

--- python/hello_world_001.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-print("This is a example with python 3 to print a text")
-print("Use the comand print('text')")
-print("""
-
-You can use diferent metods to print your text.
-With this triple " you can build a text form.
-	""")
