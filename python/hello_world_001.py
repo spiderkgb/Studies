561127318ba187582ea503fcf306f4613e03f11b
tonnytg <tonnytg@gmail.com>
2013-04-03 23:05:32 +0000

    Create a folder python, to contain python studies project

---
+++ python/hello_world_001.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+print("This is a example with python 3 to print a text")
+print("Use the comand print('text')")
+print("""
+
+You can use diferent metods to print your text.
+With this triple " you can build a text form.
+	""")
