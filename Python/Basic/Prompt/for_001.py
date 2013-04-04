fffa3fd7fb43f638bae9a2b1fb5d1c9642667e7b
tonnytg <tonnytg@gmail.com>
2013-04-04 22:27:29 +0000

    File list example are edited

--- Python/Basic/Prompt/for_001.py
+++ Python/Basic/Prompt/for_001.py
@@ -7,7 +7,7 @@ print("loop with range")
 for COUNT in range (0,4):
 	print (COUNT)
 
-print("loop when object contain value")
+print("loop when object contain value in list")
 list = ['name','years','date']
 
 for COUNT in list:
---
+++ Python/Basic/Prompt/list_001.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+print("This file create a list and show after.")
+lista = ['a','b','c','d','e']
+type( lista )
+
+print("Loop for with value inside list")
+for COUNT in lista:
+	print ( COUNT )
+
