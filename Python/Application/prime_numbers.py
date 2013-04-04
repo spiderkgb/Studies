a9b2f4566964e0e4d215f03b7015391cc8f1372a
tonnytg <tonnytg@gmail.com>
2013-04-04 22:41:49 +0000

    Invalid number alert create

--- Python/Application/prime_numbers.py
+++ Python/Application/prime_numbers.py
@@ -2,14 +2,18 @@
 # -*- coding: utf-8 -*-
 
 print("This program show if number is prime or not")
-NUMBER = int(input("Insert a number: "))
-FLAG = 0
 
-for COUNT in range (1, NUMBER+1 ):
-	if NUMBER % COUNT == 0:
-		FLAG +=1
+try:
+	NUMBER = int(input("Insert a number: "))
+	FLAG = 0
 
-if ( (FLAG <= 2) and ( NUMBER != 1) ):
-	print ("This number is prime")
-else:
-	print ("This number is not prime")
+	for COUNT in range (1, NUMBER+1 ):
+		if NUMBER % COUNT == 0:
+			FLAG +=1
+
+	if ( (FLAG <= 2) and ( NUMBER != 1) ):
+		print ("This number is prime")
+	else:
+		print ("This number is not prime")
+except:
+	print("Invalid Number!")
