71fe347d9e429fbced0d9da463b1f813b209ef96
tonnytg <tonnytg@gmail.com>
2013-04-05 00:24:25 +0000

    File with while example

---
+++ Python/Basic/Prompt/while_001.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+print("Infinite loop with whyle")
+while True:
+	input("Press to jump to next loop\n")
+	print("Rember this is a infinite loop, to exit press CTRL + z")
---
+++ Python/Basic/Prompt/while_002.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+print("The file show while with option")
+
+option = input("Press 1 to continue or another key to quit")
+print ("You press: ",option)
+
+while ( option == '1' ):
+	print("Pressed 1")
+	option = input("Press 1 to continue or another key to quit")
+
+print("To this loop with while work, you need ask two times")
