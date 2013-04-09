ce2ff314a75ff63c75286e3643f8ef8c8b96098e
tonnytg <tonnytg@gmail.com>
2013-04-09 00:19:22 +0000

    Even and Odd Number comparation are created

---
+++ Python/Application/Prompt/even_odd.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+print ("This file show even number or odd number!")
+
+NUMBER = int(input("Insert a number :"))
+
+if NUMBER % 2 == 0:
+        print ( "The number %s is odd number" % NUMBER)
+else:
+        print ( "The number %s is even number" % NUMBER)
