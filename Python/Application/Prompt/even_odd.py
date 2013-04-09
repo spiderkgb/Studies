497bff03cc86149773adbd2b54a48215c44d21a0
tonnytg <tonnytg@gmail.com>
2013-04-09 00:24:06 +0000

    Even and odd number works

--- Python/Application/Prompt/even_odd.py
+++ Python/Application/Prompt/even_odd.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 
+os.system("clear")
 print ("This file show even number or odd number!")
 
-NUMBER = int(input("Insert a number :"))
-
-if NUMBER % 2 == 0:
-        print ( "The number %s is odd number" % NUMBER)
-else:
-        print ( "The number %s is even number" % NUMBER)
+try:
+	NUMBER = int(input("Insert a number :"))
+	if NUMBER % 2 == 0:
+	        print ( "The number %s is odd number" % NUMBER)
+	else:
+	        print ( "The number %s is even number" % NUMBER)
+except:
+	print(" The value need base 10 to work!")
