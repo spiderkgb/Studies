a757db7ffbaebba26396f3a8f926ff12e9429e1c
root <root@localhost.localdomain>
2013-04-09 08:33:07 +0000

    variable example created

---
+++ Python/Basic/Prompt/variable.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+print("This file show how variable works")
+
+NAME = input("Insert a name: ")
+
+print ("\n\nFirst example of variable:\nName inserted are: ", NAME )
+print ("\n\nSecond example of variable:\nName inserted are %s." % NAME)
