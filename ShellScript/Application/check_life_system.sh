6db361567a8fe018221a5b6942af745bfc258dd5
root <root@localhost.localdomain>
2013-04-26 17:53:12 +0000

    70% ended

--- ShellScript/Application/check_life_system.sh
+++ ShellScript/Application/check_life_system.sh
@@ -19,7 +19,7 @@ ADDRESS_LOCAL="127.0.0.1";
 ADDRESS_GATEWAY="192.168.4.14";
 ADDRESS_DNS="192.168.4.15";
 
-###
+### Start Check Ping
 #	1/3 - Check ping Local
 
         if [ `ping $ADDRESS_LOCAL -c 6 | grep 64 | wc -l` -ge 2 ];then
@@ -27,10 +27,9 @@ ADDRESS_DNS="192.168.4.15";
 	else
 		echo "ERRO $ADDRESS_LOCAL Inativo";
 	fi
-
 #       2/3 - Check ping dns
 
-        if [ `ping $ADDRESS_GATEWAYL -c 6 | grep 64 | wc -l` -ge 2 ];then
+        if [ `ping $ADDRESS_GATEWAY -c 6 | grep 64 | wc -l` -ge 2 ];then
                 echo "Pingando $ADDRESS_GATEWAY";
         else
                 echo "ERRO $ADDRESS_GATEWAY Inativo";
@@ -43,12 +42,43 @@ ADDRESS_DNS="192.168.4.15";
         else
                 echo "ERRO $ADDRESS_DNS Inativo";
         fi
+#
+### End Check Ping
 
-### Fim teste ping
+### Check Memory
+#
+echo "This check count Memory and Swap memory"
 
 
+ TOTAL_MEMORY=`free -t -m | tail -n 1 | awk '{ print $2}'`;
+ MEMORY_USED=`free -t -m | tail -n 1 | awk '{ print $3}'`;
+ PERCENT=`echo $(( (100 * MEMORY_USED)/ $TOTAL_MEMORY ))`; 
+ 
+ echo "TOTAL: $TOTAL_MEMORY";
+ echo "USADA: $MEMORY_USED";
+ echo "PORCENTAGEM: $PERCENT";
 
+ if [ '$PERCENT' > '90' ];then
+  echo "Porcentagem acima $PERCENT";
+ else
+  echo "Porcentagem abaixo $PERCENT";
+ fi
 
+#
+### End Check Memory
 
+### Start Check Disk
+#
+
+ PORCENT_DISK=`df -h -l / --total | tail -n 1 | awk '{ print $5}'| cut -c1-2`;
+ echo $PORCENT_DISK;
+ if [ 'PORCENT_DISK' < 90 ];then
+  echo "ERRO Alerta de disco muito cheio";
+ else
+  echo "Disco com $PORCENT_DISK de uso";
+ fi
+
+#
+###
 
 echo "Script Check End!";
