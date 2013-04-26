f15982a4dfba32c8cfa5df04f4682580431f7a16
root <root@localhost.localdomain>
2013-04-26 16:14:39 +0000

    Folder ShellScript moved

---
+++ ShellScript/Application/check_life_system.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+###
+#	Script to check services stats
+#	Created 26/04/2013
+#	by Antonio Thomacelli Gomes
+#	http://www.linuxresort.blogspot.com.br
+###
+
+
+ADDRESS="";
+
+
+
+
+echo "Script Check End!";
