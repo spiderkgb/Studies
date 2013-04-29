0b5245a0fc2a70a91f9083e44c4640fe05d4aaba
root <root@localhost.localdomain>
2013-04-29 13:48:51 +0000

    Cpu % complete

--- ShellScript/Application/check_life_system.sh
+++ ShellScript/Application/check_life_system.sh
@@ -16,47 +16,48 @@
 ###
 
 ADDRESS_LOCAL="127.0.0.1";
-ADDRESS_GATEWAY="192.168.4.14";
-ADDRESS_DNS="192.168.4.15";
+ADDRESS_GATEWAY="127.0.0.2";
+ADDRESS_DNS="127.0.0.3";
 
 ### Start Check Ping
-#	1/3 - Check ping Local
+# 1/3 - Check ping Local
 
-        if [ `ping $ADDRESS_LOCAL -c 6 | grep 64 | wc -l` -ge 2 ];then
-		echo "Pingando $ADDRESS_LOCAL";
-	else
+ echo " Ping Check";
+ if [ `ping $ADDRESS_LOCAL -c 6 | grep 64 | wc -l` -ge 2 ];then
+  echo "Pingando $ADDRESS_LOCAL";
+ else
 		echo "ERRO $ADDRESS_LOCAL Inativo";
-	fi
-#       2/3 - Check ping dns
+ fi
 
-        if [ `ping $ADDRESS_GATEWAY -c 6 | grep 64 | wc -l` -ge 2 ];then
-                echo "Pingando $ADDRESS_GATEWAY";
-        else
-                echo "ERRO $ADDRESS_GATEWAY Inativo";
-        fi
+# 2/3 - Check ping dns
+ if [ `ping $ADDRESS_GATEWAY -c 6 | grep 64 | wc -l` -ge 2 ];then
+  echo "Pingando $ADDRESS_GATEWAY";
+ else
+  echo "ERRO $ADDRESS_GATEWAY Inativo";
+ fi
 
-#       3/3 - Check ping DNS
+# 3/3 - Check ping DNS
 
-        if [ `ping $ADDRESS_DNS -c 6 | grep 64 | wc -l` -ge 2 ];then
-                echo "Pingando $ADDRESS_DNS";
-        else
-                echo "ERRO $ADDRESS_DNS Inativo";
-        fi
-#
+ if [ `ping $ADDRESS_DNS -c 6 | grep 64 | wc -l` -ge 2 ];then
+  echo "Pingando $ADDRESS_DNS";
+ else
+  echo "ERRO $ADDRESS_DNS Inativo";
+ fi
+
 ### End Check Ping
 
 ### Check Memory
 #
-echo "This check count Memory and Swap memory"
-
-
+#echo "This check count Memory and Swap memory"
+ echo "";
+ echo "Memory Check";
  TOTAL_MEMORY=`free -t -m | tail -n 1 | awk '{ print $2}'`;
  MEMORY_USED=`free -t -m | tail -n 1 | awk '{ print $3}'`;
  PERCENT=`echo $(( (100 * MEMORY_USED)/ $TOTAL_MEMORY ))`; 
  
- echo "TOTAL: $TOTAL_MEMORY";
- echo "USADA: $MEMORY_USED";
- echo "PORCENTAGEM: $PERCENT";
+# echo "TOTAL: $TOTAL_MEMORY";
+# echo "USADA: $MEMORY_USED";
+# echo "PORCENTAGEM: $PERCENT";
 
  if [ '$PERCENT' > '90' ];then
   echo "Porcentagem acima $PERCENT";
@@ -69,16 +70,32 @@ echo "This check count Memory and Swap memory"
 
 ### Start Check Disk
 #
-
+ echo "";
+ echo "Disk Check";
  PORCENT_DISK=`df -h -l / --total | tail -n 1 | awk '{ print $5}'| cut -c1-2`;
  echo $PORCENT_DISK;
- if [ 'PORCENT_DISK' < 90 ];then
+ if [ $PORCENT_DISK -ge '10' ];then
   echo "ERRO Alerta de disco muito cheio";
  else
-  echo "Disco com $PORCENT_DISK de uso";
+  echo "Disco OK, com $PORCENT_DISK % de uso";
  fi
 
 #
-###
+### End Check Disk
+
+### Start Check CPU
+#
+ echo "";
+ echo "CPU Check";
+ PORCENT_CPU=`top -b -n1 | tail -n+8 | sort -nr -k9 | awk '{ print $9}' | head -n 1`;
+ if [ $PORCENT_CPU -ge '10' ];then
+  echo "ERRO Alerta CPU acima do normal";
+ else
+  echo "CPU OK, com $PORCENT_CPU % de uso";
+ fi
+
+
+#
+### End Check CPU
 
 echo "Script Check End!";
