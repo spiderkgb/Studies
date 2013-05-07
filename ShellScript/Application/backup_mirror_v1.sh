37e390c6cb704e9f1ab7be09bc8f29f00843a52e
root <root@localhost.localdomain>
2013-05-07 08:53:40 +0000

    Backup script creating

---
+++ ShellScript/Application/backup_mirror_v1.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+###
+#	Backup script
+#	Check a destiny and create a mirror
+#	This script use 
+#	by Antonio Thomacelli Gomes
+#	Date 07/06/2013
+#	Edited 07/06/2013
+#	Version 1.0
+###
+
+
+
+###
