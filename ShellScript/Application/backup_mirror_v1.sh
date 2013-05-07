73f34c44a6d5b145554f073b12a950300e0388fa
root <root@localhost.localdomain>
2013-05-07 09:37:47 +0000

    Loop for to check file created

--- ShellScript/Application/backup_mirror_v1.sh
+++ ShellScript/Application/backup_mirror_v1.sh
@@ -3,32 +3,36 @@
 ###
 #	Backup script
 #	Check a destiny and create a mirror
-#	This script use "rsync"
+#	This script use "rsync" and KeySSH
+#	
 #	by Antonio Thomacelli Gomes
 #	Date 07/06/2013 | Edited 07/06/2013 | Version 1.0
 ###
 ####
+
 ###
 #	Start Variable
 ###
 
-
-
+ADDRESS_LIST="./list.txt"; # <- Create a file with address to check
+ADDRESS_FILE="";
+TOTAL_ITEM_CHECK=`wc -l $ADDRESS_LIST | awk '{ print $1}'`;
+DAY=`date +%d`;
+MONTH=`date +%m`;
+YEAR=`date +%Y`;
 ###	End Variable
 ####
+
+clear;
+for ((i=1; i<$TOTAL_ITEM_CHECK+1;i++))
+do
+	DESTINY=`sed -n $i' p;' $ADDRESS_LIST`;
+	echo $DESTINY;
+done
+
+
+
+
+
 ###
-#	Start Check List
-###
-
-
-
-###	End Check List
-####
-###
-#	Start Backup
-###
-
-
-
-###	End Backup
 ####	End Script
