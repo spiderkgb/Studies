1cadb7c202289caead75c43c22c497e868a9e59f
tonnytg <tonnytg@gmail.com>
2013-05-07 13:29:08 +0000

    polls_question edited

--- Django/polls_question/polls_question/settings.py
+++ Django/polls_question/polls_question/settings.py
@@ -1,5 +1,8 @@
 # Django settings for polls_question project.
 
+import os
+
+#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -111,6 +114,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    #os.path.join(BASE_DIR, 'templates')
+    'templates',
 )
 
 INSTALLED_APPS = (
