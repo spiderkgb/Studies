f9afa43da861b795cae16c8bb8aff2ff08a086dd
tonnytg <tonnytg@gmail.com>
2013-05-28 12:53:01 -0300

    test setup python3 layout

---
+++ Python/Basic/Prompt/test_setup_python3/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup (name='test',
+ version='1.0.0',
+ descriptio='Python test setup',
+ author='Antonio',
+ author_email='tonnytg@gmail.com',
+ url='http://linuxresort.blogspot.com.br',
+ packages=['test'],
+ ) 
